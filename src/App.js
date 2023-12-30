4104300b75943b5a3c9bdb6fef55afaaec735a50
unknown <nurmansyahbachtyar@gmail.com>
2023-12-31 04:30:16 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Countdown from "react-countdown";
+import Particles from "react-particles";
+import { Typewriter } from "react-simple-typewriter";
+import { loadFireworksPreset } from "tsparticles-preset-fireworks";
 
 function App() {
+  const [newYearMessage, setNewYearMessage] = useState(["Waiting for 2024..."]);
+
+  const particleInitialization = async (engine) => {
+    await loadFireworksPreset(engine);
+  };
+
+  function timeLeft() {
+    const newYearDate = new Date("Januari 1, 2024 00:00:00").getTime();
+    const nowDate = new Date().getTime();
+    const remaininTime = newYearDate - nowDate;
+    return remaininTime;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Particles
+        init={particleInitialization}
+        options={{ preset: "fireworks" }}
+      />
+      <div className="flex flex-col justify-center items-center min-h-screen gap-4">
+        <span className="text-white text-4xl font-bold px-4 z-50">
+          <Typewriter
+            words={newYearMessage}
+            loop={false}
+            cursorStyle={"🎉"}
+            cursor
+          />
+        </span>
+        <div className="z-50 text-white font-bold text-2xl">
+          <Countdown
+            date={Date.now() + timeLeft()}
+            onComplete={() =>
+              setNewYearMessage([
+                "Selamat Tahun Baru 2024! 🎉🎉 ",
+                "Semoga segala hal terbaik terjadi padamu..",
+              ])
+            }
+          />
+        </div>
+      </div>
+    </>
   );
 }
 
